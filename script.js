d2f9fa4712dd73ac98831a71c3a55a429787000b
klballard <kyleleeballard@gmail.com>
2020-09-25 14:13:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const baseURL = " https://pokeapi.co/api/v2/pokemon/";
+
+let url;
+
+// search field
+const searchTerm = document.querySelector('.search');
+const searchForm = document.querySelector('form');
+const submitBtn = document.querySelector('.submit');
+
+
+
+searchForm.addEventListener('submit', fetchResults);
+
+//fetching from the api
+function fetchResults(e) {
+    e.preventDefault();
+    url = baseURL + searchTerm.value;
+    fetch(url)
+        .then(function(result) {
+            return result.json();
+        }).then(function(json) {
+            displayResults(json);
+        });
+
+}
+
+function displayResults(json) {
+    console.log("DisplayResults", json);
+}
+
+function displayResults(json) {
+    console.log("DisplayResults", json);
+    document.getElementById('pokeName').innerHTML = json.name;
+    document.getElementById('pokeNumber').innerHTML = `pokedex #` + json.id;
+    document.getElementById('pokeImage').src = json.sprites.front_default;
+    document.getElementById('pokeShiny').src = json.sprites.front_shiny;
+    document.getElementById('pokeHealth').innerHTML = json.stats[0].base_stat;
+    document.getElementById('pokeAttack').innerHTML = json.stats[1].base_stat;
+    document.getElementById('pokeDefense').innerHTML = json.stats[2].base_stat;
+    document.getElementById('pokeSpAttack').innerHTML = json.stats[3].base_stat;
+    document.getElementById('pokeSpDefense').innerHTML = json.stats[4].base_stat;
+    document.getElementById('pokeSpeed').innerHTML = json.stats[5].base_stat;
+};
