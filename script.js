bfb6016fef4d1b70017ef521d4a7f39023208f75
klballard <kyleleeballard@gmail.com>
2020-09-28 09:03:54 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -31,13 +31,14 @@ function displayResults(json) {
 function displayResults(json) {
     console.log("DisplayResults", json);
     document.getElementById('pokeName').innerHTML = json.name;
-    document.getElementById('pokeNumber').innerHTML = `pokedex #` + json.id;
+    document.getElementById('pokeName').style.textTransform = "capitalize";
+    document.getElementById('pokeNumber').innerHTML = `Pokedex #` + json.id;
     document.getElementById('pokeImage').src = json.sprites.front_default;
     document.getElementById('pokeShiny').src = json.sprites.front_shiny;
-    document.getElementById('pokeHealth').innerHTML = json.stats[0].base_stat;
-    document.getElementById('pokeAttack').innerHTML = json.stats[1].base_stat;
-    document.getElementById('pokeDefense').innerHTML = json.stats[2].base_stat;
-    document.getElementById('pokeSpAttack').innerHTML = json.stats[3].base_stat;
-    document.getElementById('pokeSpDefense').innerHTML = json.stats[4].base_stat;
-    document.getElementById('pokeSpeed').innerHTML = json.stats[5].base_stat;
+    document.getElementById('pokeHealth').innerHTML = `HP: ` + json.stats[0].base_stat;
+    document.getElementById('pokeAttack').innerHTML = `Attack: ` + json.stats[1].base_stat;
+    document.getElementById('pokeDefense').innerHTML = `Defense: ` + json.stats[2].base_stat;
+    document.getElementById('pokeSpAttack').innerHTML = `Sp. Attack: ` + json.stats[3].base_stat;
+    document.getElementById('pokeSpDefense').innerHTML = `Sp. Defense: ` + json.stats[4].base_stat;
+    document.getElementById('pokeSpeed').innerHTML = `Speed: ` + json.stats[5].base_stat;
 };
